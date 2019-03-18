0360aee7c382ffc72766ae344e463de7eecbf193
Keizaak <37865090+Keizaak@users.noreply.github.com>
2019-03-18 13:59:00 +0100

    - Ajout de la map (markers, rayon, permissions)

--- app/src/main/java/fr/iutbm/dontwaste/MapFragment.java
+++ app/src/main/java/fr/iutbm/dontwaste/MapFragment.java
@@ -1,12 +1,40 @@
 package fr.iutbm.dontwaste;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -17,7 +45,7 @@ import android.view.ViewGroup;
  * Use the {@link MapFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -28,6 +56,22 @@ public class MapFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    private AppDatabase appDatabase;
+    private MealDAO mealDAO;
+    private List<Meal> mealList = new ArrayList<>();
+
+    private GoogleApiClient mGoogleApiClient = null;
+
+    private static int REQUEST_LOCATION = 1;
+
+    private Location mLastLocation;
+
+    private boolean mRequestLocationUpdates;
+    private LocationRequest mLocationRequest;
+
+    private GoogleMap mGoogleMap;
+
 
     public MapFragment() {
         // Required empty public constructor
@@ -58,13 +102,64 @@ public class MapFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        if(mGoogleApiClient == null){
+            mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        appDatabase = AppDatabase.getDatabase(getContext());
+        mealDAO = appDatabase.mealDAO();
+        (new GetAllMealsMapAsyncTask(mealDAO)).execute();
+
+        View root = inflater.inflate(R.layout.fragment_map, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        return root;
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        setLocationParameters();
+        if(mGoogleApiClient.isConnected()){
+            if(mRequestLocationUpdates){
+                startLocationUpdates();
+            } else {
+                stopLocationUpdates();
+            }
+        }
+    }
+
+    protected void startLocationUpdates(){
+        if(ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestLocationPermission();
+        } else {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+    }
+
+    protected void stopLocationUpdates(){
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -91,6 +186,99 @@ public class MapFragment extends Fragment {
         mListener = null;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        //mGoogleMap.setOnMarkerClickListener(this);
+
+        if(!mealList.isEmpty()){
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+            for(Meal meal : mealList){
+                LatLng pos = new LatLng(meal.getLatitude(), meal.getLongitude());
+                builder.include(pos);
+                LatLngBounds bounds = builder.build();
+                mGoogleMap.addMarker(new MarkerOptions().position(pos).title(meal.getMealName()).alpha(0.5f));
+                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100));
+            }
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if(ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestLocationPermission();
+        } else {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if(mLastLocation != null){
+                String lats = "" + mLastLocation.getLatitude();
+                String longs = "" + mLastLocation.getLongitude();
+                Toast.makeText(getActivity(), lats + " " + longs, Toast.LENGTH_LONG).show();
+            }
+        }
+
+        if(mRequestLocationUpdates){
+            startLocationUpdates();
+        } else {
+            stopLocationUpdates();
+        }
+    }
+
+    private void requestLocationPermission(){
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+    }
+
+    public void setLocationParameters(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mRequestLocationUpdates = sharedPreferences.getBoolean(getResources().getString(R.string.key_location_switch), false);
+
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(Integer.parseInt(sharedPreferences.getString(getResources().getString(R.string.key_search_delay), "100")));
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        if(location != null){
+            Toast.makeText(getActivity(), location.getLatitude() + " " + location.getLongitude(), Toast.LENGTH_LONG).show();
+
+            int radius = Integer.parseInt(sharedPreferences.getString(getResources().getString(R.string.key_search_radius), "10"));
+
+            mGoogleMap.clear();
+            LatLngBounds.Builder builder = LatLngBounds.builder();
+
+            for(Meal meal : mealList){
+                LatLng pos = new LatLng(meal.getLatitude(), meal.getLongitude());
+                builder.include(pos);
+
+                MarkerOptions markerOptions = new MarkerOptions().position(pos).title(meal.getMealName()).alpha(0.5f);
+
+                Location mealLocation = new Location("");
+                mealLocation.setLatitude(meal.getLatitude());
+                mealLocation.setLongitude(meal.getLongitude());
+
+                if(location.distanceTo(mealLocation) < radius){
+                    markerOptions.alpha(1);
+                }
+
+                mGoogleMap.addMarker(markerOptions);
+            }
+
+            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 100));
+        }
+    }
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -104,5 +292,25 @@ public class MapFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    private class GetAllMealsMapAsyncTask extends AsyncTask<Void, Void, Void>{
+        private MealDAO mAsyncTaskDao;
+        ArrayList<Meal> meals;
+
+        GetAllMealsMapAsyncTask(MealDAO dao){
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids){
+            meals = new ArrayList<>(mAsyncTaskDao.getAllMeals());
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void voids){
+            mealList = meals;
+        }
     }
 }
