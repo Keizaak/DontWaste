584c4f3fa930f1e6285e5678697ba3da05981290
Chloé Vieille <chloevieille@hotmail.com>
2019-03-28 19:24:10 +0100

    Redirection sur boite mail au moment d'envoyer

--- app/src/main/java/fr/iutbm/dontwaste/ContactFormView.java
+++ app/src/main/java/fr/iutbm/dontwaste/ContactFormView.java
@@ -1,13 +1,47 @@
 package fr.iutbm.dontwaste;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ContactFormView extends AppCompatActivity {
+    private EditText textTo;
+    private EditText textSubject;
+    private EditText textMessage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_form_view);
+
+        textSubject = findViewById(R.id.txtSubject);
+        textMessage = findViewById(R.id.txtMessage);
+
+        Button buttonSend = findViewById(R.id.btnSend);
+        buttonSend.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                sendMail();
+            }
+        });
+    }
+
+    private void sendMail(){
+        String sendTo = "testemailaddress@testtest.com";
+        String subject = textSubject.getText().toString();
+        String message = textMessage.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("message/rfc822");
+
+        intent.putExtra(Intent.EXTRA_EMAIL, sendTo);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        
+        startActivity(Intent.createChooser(intent, "Contact mail"));
     }
 }
